7611cacadaec0ea8c62a6343e32a6b9ad41b796b
luismagnum <luismagnum1@gmail.com>
2024-08-12 11:10:33 -0300

    titulos

--- src/components/idiomas/Idiomas.jsx
+++ src/components/idiomas/Idiomas.jsx
@@ -4,8 +4,8 @@ import br from '../../assets/brflag.png'
 
 const Idiomas = () => {
     return (
-      <div className='text-center justify-items-center mx-auto mt-16 mb-10 px-4 md:px-8'>
-        <h1 className='text-3xl text-gray-600 font-bold mb-10'>Idiomas</h1>
+      <div className='text-center justify-items-center mx-auto mt-0 md:mt-10 mb-10 px-4 md:px-8'>
+        <h1 className='text-3xl text-gray-600 font-bold mb-4'>Idiomas</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
           <div className='text-center mx-auto w-full md:w-3/4 p-4 bg-white rounded-lg shadow-md'>
             <img src={br} alt='' className='mx-auto'/>
