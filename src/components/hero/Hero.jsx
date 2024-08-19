9dc52a9297f57f25b147e92282ceb0ece7a02b26
luismagnum <luismagnum1@gmail.com>
2024-08-19 17:23:59 -0300

    text color

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className ='w-full h-80  md:min-h-screen  flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(assets/survey.jpg)] bg-contain md:bg-cover bg-no-repeat'>
       <div className='w-full ml-0 space-y-5 px-5 lg:px-0 mb-10 lg:mb-28'>
-        <h1 className='text-left text-sky-900 font-bold text-2xl md:text-6xl'>CURSOS</h1>
+        <h1 className='text-left text-sky-700 font-bold text-2xl md:text-6xl'>CURSOS</h1>
         <p className=' text-sky-900 w-1/3 text-base md:text-4xl font-bold'>
           Educacion y aprendizaje en linea.
         </p>
